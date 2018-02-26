d1c23ace824ebc87543d529594ecf3f9174e946d
bitsnaps <bitsnaps@yahoo.fr>
2018-02-26 15:02:34 +0100

    generating random values
    I've just made values generated randomly to see different results.

--- demo.py
+++ demo.py
@@ -1,26 +1,30 @@
+# Desicion tree example
+
+# Dependencies:
+# Scikit-learn (http://scikit-learn.org/stable/install.html)
+# numpy (pip install numpy)
+# scipy (pip install scipy)
+
 from sklearn import tree
-
-clf = tree.DecisionTreeClassifier()
-
-# CHALLENGE - create 3 more classifiers...
-# 1
-# 2
-# 3
+import random as r
 
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
-
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
+     
+print("Size: %d" % len(X))
 
+# Create a classifier
+clf = tree.DecisionTreeClassifier()
 
-# CHALLENGE - ...and train them on our data
 clf = clf.fit(X, Y)
 
-prediction = clf.predict([[190, 70, 43]])
+# Generating random values
+values = [r.randint(150,220), r.randint(50,130), r.randint(37,50)]
+prediction = clf.predict([values])
 
-# CHALLENGE compare their reusults and print the best one!
-
+print("Prediction for %s " % values)
 print(prediction)
